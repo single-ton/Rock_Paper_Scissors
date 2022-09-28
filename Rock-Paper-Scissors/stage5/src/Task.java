5eb9f047b778ff2581e0d23e844148fd32ce7afe
single-ton <antoniousenko@gmail.com>
2022-09-29 01:53:45 +0300

    change stage 5

--- Rock-Paper-Scissors/stage5/src/Task.java
+++ Rock-Paper-Scissors/stage5/src/Task.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Time;
-import java.sql.Timestamp;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Task {
@@ -10,33 +14,100 @@ public class Task {
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
-        // Reading input
-        String input = reader.readLine().toLowerCase();
-        Random r = new Random(System.currentTimeMillis());
-        int index = r.nextInt(2);
-        String randomResult = cases[index];
-        if(input.equals(randomResult))
-            System.out.printf("There is a draw (%s)\n", randomResult);
-        else if(getWin(input).equals(randomResult))
-            System.out.printf( "Sorry, but the computer chose %s\n", randomResult);
-        else if(input.equals(getWin(randomResult)))
-            System.out.printf("Well done. The computer chose %s and failed\n", randomResult);
+        System.out.print("Enter your name:");
+        String name = reader.readLine();
+        System.out.printf("Hello, %s%n", name);
 
-    }
-    static String[] cases = new String[]{"rock",
-            "paper",
-            "scissors"};
-    static String getWin(String input){
-        for(int i=0;i<cases.length;i++)
-            if(input.equals(cases[i])){
-                if(i+1<cases.length)
-                    return cases[i+1];
-                else
-                    return cases[0];
+        Path path = Paths.get("rating.txt");
+        List<String> listStrings = Files.readAllLines(path);
+        int rating = 0;
+        for (String s : listStrings) {
+            if (s.split(" ")[0].equals(name))
+                rating = Integer.parseInt(s.split(" ")[1]);
+        }
+        String basic = reader.readLine();
+        Map<String, String> options = new HashMap<String, String>();
+        if(basic.equals("")) {
+            options.put("scissors", "rock");
+            options.put("rock", "paper");
+            options.put("paper", "scissors");
+        }
+        else {
+            String[] list={"lightning", "gun", "air", "water", "dragon", "paper", "devil"};
+            options.put("rock", String.join("\n",list));
+            list=new String[]{"lightning", "sponge", "air", "water", "dragon", "paper", "devil"};
+            options.put("gun", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "air", "water", "dragon", "paper", "devil"};
+            options.put("lightning", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "air", "water", "dragon", "paper", "tree"};
+            options.put("devil", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "air", "water", "human", "paper", "tree"};
+            options.put("dragon", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "air", "snake", "human", "paper", "tree"};
+            options.put("water", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "scissors", "snake", "human", "paper", "tree"};
+            options.put("air", String.join("\n",list));
+            list=new String[]{"wolf", "sponge", "scissors", "snake", "human", "fire", "tree"};
+            options.put("paper", String.join("\n",list));
+            list=new String[]{"wolf", "rock", "scissors", "snake", "human", "fire", "tree"};
+            options.put("sponge", String.join("\n",list));
+            list=new String[]{"gun", "rock", "scissors", "snake", "human", "fire", "tree"};
+            options.put("wolf", String.join("\n",list));
+            list=new String[]{"gun", "rock", "scissors", "snake", "human", "fire", "lightning"};
+            options.put("tree", String.join("\n",list));
+            list=new String[]{"gun", "rock", "scissors", "snake", "devil", "fire", "lightning"};
+            options.put("human", String.join("\n",list));
+            list=new String[]{"gun", "rock", "scissors", "dragon", "devil", "fire", "lightning"};
+            options.put("snake", String.join("\n",list));
+            list=new String[]{"gun", "rock", "water", "dragon", "devil", "fire", "lightning"};
+            options.put("scissors", String.join("\n",list));
+            list=new String[]{"lightning", "gun", "air", "water", "dragon", "rock", "devil"};
+            options.put("fire", String.join("\n",list));
+
+        }
+        Random r = new Random();
+        int len = options.keySet().size();
+        int randIndex = r.nextInt(0,len);
+
+        System.out.println("Okay, let's start");
+        while(true){
+            randIndex = r.nextInt(0,len);
+            String option="121212121";
+            int i=0;
+            for(String s:options.keySet()){
+                if(i==randIndex) {
+                    option = s;
+                    break;
+                }
+                i++;
             }
-        return "NotExist";
-    }
-    static String getOutput(String input){
-        return String.format("Sorry, but the computer chose %s",getWin(input));
+
+            String hand = reader.readLine();
+
+            if(options.containsKey(hand)) {
+                if(options.size()>3){
+                    i++;
+                    i--;
+                }
+                if (hand.equals(option)) {
+                    System.out.printf("There is a draw (%s)\n", option);
+                    rating += 50;
+                } else if (options.get(option).contains(hand)) {
+                    System.out.printf("Well done. The computer chose %s and failed\n", option);
+                    rating+=100;
+                }
+                else
+                    System.out.printf("Sorry, but the computer chose %s\n",option);
+            }
+            else if(hand.equals("!exit")){
+                System.out.println("Bye!");
+                break;
+            } else if (hand.equals("!rating")) {
+                System.out.printf("Your rating: %s\n",rating);
+            }
+            else
+                System.out.println("Invalid input");
+        }
+
     }
 }
